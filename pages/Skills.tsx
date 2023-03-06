24b7cd0f480511b442a92490015f47a1d5fa537e
Rushit Patel <rushitpatel163@gmail.com>
2023-03-06 20:44:31 +0530

    Basic Background Added

---
+++ pages/Skills.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className=' h-[100vh] bg-slate-900'>
+      
+    </div>
+  )
+}
+
+export default Skills;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import About from './About'
+import Skills from './Skills'
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 //marquee effect
 const Home: NextPage = () => {
   return (
@@ -9,7 +11,14 @@ const Home: NextPage = () => {
     <title>Rushit Patel</title>
     <meta property="og:title" content="My page title" key="title" />
     </Head>
-    <About/>
+    <Parallax pages={2.5}>
+    <ParallaxLayer  offset={0} speed={1} sticky={{ start: 0, end: 1 }}>
+      <About/>
+    </ParallaxLayer>
+    <ParallaxLayer  offset={0.9999} speed={1} sticky={{ start: 1, end: 0 }}>
+      <Skills/>
+    </ParallaxLayer>
+    </Parallax>
   </>
   )
 }
