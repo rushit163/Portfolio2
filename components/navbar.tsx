86a56db78037b74424596db9151340b1b6449353
Rushit Patel <rushitpatel163@gmail.com>
2023-03-09 10:43:19 +0530

    Navbar Added Successfully

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,24 +3,22 @@ import React from 'react'
 const Navbar = () => {
   return (
       
-<nav className="bg-transperant px-2 sm:px-4 py-2.5  fixed w-full z-20 top-0 left-0">
-  <div className="container flex flex-wrap items-center justify-center mx-auto">
-  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+<nav className="flex flex-row item-center justify-center bg-transperant px-2 sm:px-4 py-2.5  fixed w-full z-20 top-0 left-0">
+  <div className=" w-full">
+    <ul className="flex flex-row item-center justify-center p-4 mt-4 rounded-lg  ">
       <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-700  bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white">Home</a>
+        <a href="#" className=" py-2 pl-3 pr-4 text-gray-700  rounded dark:text-white">Home</a>
       </li>
       <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+        <a href="#" className=" py-2 pl-3 pr-4 text-gray-700  rounded dark:text-white">About</a>
       </li>
       <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+        <a href="#" className=" py-2 pl-3 pr-4 text-gray-700  rounded dark:text-white">Services</a>
       </li>
       <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+        <a href="#" className=" py-2 pl-3 pr-4 text-gray-700  rounded dark:text-white">Contact</a>
       </li>
     </ul>
-  </div>
   </div>
 </nav>
 
