5e7062e205fbcfa3d7af0f78dfd703387a8a22f3
Rushit Patel <rushitpatel163@gmail.com>
2023-03-09 21:49:26 +0530

    Navbar is not collapsable

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,49 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 const Navbar = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true)
+    
+    const handleScroll = () => {
+        const currentScrollPos = window.scrollY
+    
+        if(currentScrollPos > prevScrollPos){
+            setVisible(false)
+        }else{
+            setVisible(true)
+        }
+    
+        setPrevScrollPos(currentScrollPos)
+    }
+    
+    useEffect( () => {
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => window.removeEventListener('scroll', handleScroll)
+    })
   return (
-      
-<nav className="flex flex-row item-center justify-center bg-transperant px-2 sm:px-4 py-2.5  fixed w-full z-20 top-0 left-0">
-  <div className=" w-full">
-    <ul className="flex flex-row item-center justify-center p-4 mt-4 rounded-lg  ">
-      <li>
-        <a href="#" className=" py-2 pl-3 pr-4 text-gray-700  rounded dark:text-white">Home</a>
-      </li>
-      <li>
-        <a href="#" className=" py-2 pl-3 pr-4 text-gray-700  rounded dark:text-white">About</a>
-      </li>
-      <li>
-        <a href="#" className=" py-2 pl-3 pr-4 text-gray-700  rounded dark:text-white">Services</a>
-      </li>
-      <li>
-        <a href="#" className=" py-2 pl-3 pr-4 text-gray-700  rounded dark:text-white">Contact</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+    
+    <>
+    <nav className={`flex flex-row item-center justify-center bg-transperant px-2  fixed w-full z-20 left-0" id='navbar`}>
+        <div className=" w-full">
+            <ul className="flex flex-row item-center justify-center p-4 mt-4 rounded-lg  ">
+            <li>
+                <a href="#" className=" py-2 pl-3 text-gray-700  rounded dark:text-white">Home</a>
+            </li>
+            <li>
+                <a href="#" className=" py-2 pl-3 text-gray-700  rounded dark:text-white">About</a>
+            </li>
+            <li>
+                <a href="#" className=" py-2 pl-3 text-gray-700  rounded dark:text-white">Services</a>
+            </li>
+            <li>
+                <a href="#" className=" py-2 pl-3 text-gray-700  rounded dark:text-white">Contact</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
 
-
+    </>
   )
 }
 
--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
           <Link href='/contactme'><p className="font-bold  text-2xl mx-4 my-3 text-slate-400 rounded-lg hover:text-slate-50">Skills</p></Link>
           <Link href='/contactme'><p className="font-bold  text-2xl mx-4 my-3 text-slate-400 rounded-lg hover:text-slate-50">Contact Me</p></Link>
       </div> */}
-    <div className="flex flex-row justify-baseline align-center h-[100vh] w-[100vw] ">
+    <div className="flex flex-row justify-baseline align-center h-[200vh] w-[100vw] ">
       <div className='flex flex-row justify-center align-center w-[50vw] mx-9 my-9'>
           <div className='text-white text-lg'>
             MY TIMELINE
