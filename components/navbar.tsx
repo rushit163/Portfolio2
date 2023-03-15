33734780332c440c6794cf61fd2a46131eb32f5e
Rushit Patel <rushitpatel163@gmail.com>
2023-03-15 10:49:33 +0530

    daily check

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,16 +5,16 @@ const Navbar = () => {
   return (
     <>
     <nav className={`flex flex-row item-center justify-center bg-transperant  w-full  fixed ${ scrollDirection === "down" ? "-top-20" : "top-0"}  z-20 `}>
-        <div className='bg-white rounded bg-opacity-50 rounded-t-none'>
-            <ul className="flex flex-row item-center justify-center  mt-2 mb-2 rounded-lg ">
+        <div className='bg-white rounded-lg bg-opacity-50 rounded-t-none'>
+            <ul className="flex flex-row item-center justify-center  mt-2 mb-2">
             <li>
                 <a href="#" className="text-l lg:text-2xl px-2 font-semibold lg:px-5 md:px-3 sm:px-2  text-slate-300  hover:text-white">Home</a>
             </li>
             <li>
-                <a href="#" className="text-l lg:text-2xl px-2 font-semibold lg:px-5  md:px-3 sm:px-2  text-slate-300  hover:text-white">About</a>
+                <a href="#" className="text-l lg:text-2xl px-2 font-semibold lg:px-5 md:px-3 sm:px-2  text-slate-300  hover:text-white">About</a>
             </li>
             <li>
-                <a href="#" className="text-l lg:text-2xl font-semibold lg:px-5 md:px-3 sm:px-2  text-slate-300  hover:text-white">Projects</a>
+                <a href="#" className="text-l lg:text-2xl px-2 font-semibold lg:px-5 md:px-3 sm:px-2  text-slate-300  hover:text-white">Projects</a>
             </li>
             <li>
                 <a href="#" className="text-l lg:text-2xl px-2 font-semibold lg:px-5  md:px-3 sm:px-2 text-slate-300  hover:text-white">Contact</a>
--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Link from 'next/link';
 const Skills = () => {
   return (
-    <div className='bg-slate-900'>
-    <div className="flex flex-row justify-baseline align-center h-[100vh] w-[100vw]">
+    
+    <div className="flex flex-row justify-baseline align-center h-[100vh] w-[100vw] bg-slate-900">
       <div className='flex flex-row justify-center align-center w-[50vw] mt-9 pt-4'>
           <div className='text-white text-lg'>
-            Timeline
+            About Me
           </div>
       </div>
       <div className='flex flex-row justify-center align-center w-[50vw] mt-9 pt-4'>
@@ -14,7 +14,6 @@ const Skills = () => {
             Let me brag about me
           </div>
       </div>
-    </div>
     </div>
   )
 }
